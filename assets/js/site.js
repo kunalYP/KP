96de9d6e7023d0de0097bd4873bb6ad9fcfb7142
kunalYP <69045235+kunalYP@users.noreply.github.com>
2020-11-07 00:03:46 -0800

    Add files via upload

---
+++ assets/js/site.js
@@ -0,0 +1,68 @@
+function sidebar_open() {
+
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+
+
+}
+
+function sidebar_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+
+}
+function addLocationsidebar_open() {
+
+    document.getElementById("addLocationSidebar").style.display = "block";
+    document.getElementById("myOverlay2").style.display = "block";
+
+
+}
+
+function addLocationsidebar_close() {
+    document.getElementById("addLocationSidebar").style.display = "none";
+    document.getElementById("myOverlay2").style.display = "none";
+
+}
+function sortsidebar_open() {
+
+  document.getElementById("sortsidebar").style.display = "block";
+  document.getElementById("myOverlay3").style.display = "block";
+
+
+}
+
+function sortsidebar_close() {
+  document.getElementById("sortsidebar").style.display = "none";
+  document.getElementById("myOverlay3").style.display = "none";
+
+}
+function filtersidebar_open() {
+
+  document.getElementById("filtersidebar").style.display = "block";
+  document.getElementById("myOverlay4").style.display = "block";
+
+
+}
+
+function filtersidebar_close() {
+  document.getElementById("filtersidebar").style.display = "none";
+  document.getElementById("myOverlay4").style.display = "none";
+
+}
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("searchbox");
+var filters = document.getElementById("filtersection");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    filters.classList.add("stickyfilter")
+  } else {
+    navbar.classList.remove("sticky");
+    filters.classList.remove("stickyfilter");
+  }
+}
